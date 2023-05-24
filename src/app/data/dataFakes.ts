f0cd8744c30d0b0a4f0a1742e7780cb8d1210ad4
Cleinaldo de  Souza de Almeida Silva <scleinaldo85@gmail.com>
2023-05-24 12:35:47 -0300

    :up:update and created content

---
+++ src/app/data/dataFakes.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    id: '1',
+    title: 'Saiu novas bibliotecas',
+    description:
+      'Você não vai ter mais problemas em relação ao que sua aplicação tem ou não tem, e o desenvolvimento vai seguir com mais confiança e agilidade, por poder testar as mudanças em diversos cenários.',
+    photoCover:
+      'https://studiolacosanostra.github.io/2019/07/19/Build-a-reusable-Angular-library-and-web-component/title-image.png',
+  },
+  {
+    id: '2',
+    title: 'Atualização bibliotecas disponíveis',
+    description: 'Bibliotecas Angular para Guias Interativos de Onboarding',
+    photoCover:
+      'https://studiolacosanostra.github.io/2019/07/19/Build-a-reusable-Angular-library-and-web-component/title-image.png',
+  },
+  {
+    id: '3',
+    title: 'Saiu novas bibliotecas',
+    description:
+      'Você não vai ter mais problemas em relação ao que sua aplicação tem ou não tem, e o desenvolvimento vai seguir com mais confiança e agilidade, por poder testar as mudanças em diversos cenários.',
+    photoCover:
+      'https://studiolacosanostra.github.io/2019/07/19/Build-a-reusable-Angular-library-and-web-component/title-image.png',
+  },
+  {
+    id: '4',
+    title: 'Atualização bibliotecas disponíveis',
+    description: 'Bibliotecas Angular para Guias Interativos de Onboarding',
+    photoCover:
+      'https://studiolacosanostra.github.io/2019/07/19/Build-a-reusable-Angular-library-and-web-component/title-image.png',
+  },
+];
